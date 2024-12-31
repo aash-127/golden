07e41db57ab6154d5301c9c09948024c3473bff4
aash-127 <arng127@gmail.com>
2024-12-31 14:41:47 +0300

    templet-engine

---
+++ assist/js/hamburger-menu.js
@@ -0,0 +1,13 @@
+const menu_btn = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
+menu_btn.addEventListener("click", function () {
+  menu_btn.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
+});
+
+document.querySelectorAll(".mobile-nav a").forEach((item) => {
+  item.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
+});
